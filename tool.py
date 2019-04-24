2191947e76c2ef41344922c4d0fc96a55b261b5f
王瑞 <15034005712@163com>
2019-04-24 12:51:46 +0800

    add photos

--- tool.py
+++ tool.py
@@ -131,7 +131,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("../wangrui15034005712.github.io/source/photos/data.json","w") as fp:
+    with open("../Hexo/source/photos/data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():

1c8eb7243208e618d541c5daf9391a910b25a4b8
allen tai <allen5183@gmail.com>
2020-08-18 15:49:45 +0800

    add photos

--- tool.py
+++ tool.py
@@ -131,7 +131,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻轉
     final_dict = {"list": list_info}
-    with open("E:\htdocs\www\git\hexo\blog\source\photos\data.json","w") as fp:
+    with open("E:/htdocs/www/git/hexo/blog/source/photos/data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
